c5ae68c3b42e78f0bdffe1597c31c406a1995cdd
unknown <luizpaulo.redes@gmail.com>
2020-11-30 09:30:39 -0300

    working with hooks instead of classes

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
+
+    const [activeIndex,setActiveIndex] = useState(null);
+
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index)
+        
+    }
+
+    const renderedItems = items.map((item,index) => {
         return <div key={item.title}>
-            <div className="title active">
+            <div 
+            className="title active"
+            onClick={()=> onTitleClick(index)}
+            >
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
@@ -15,6 +27,7 @@ const Accordion = ({ items }) => {
 
     return (<div className="ui styled accordion">
         {renderedItems}
+        <h1>{activeIndex}</h1>
     </div>
     )
 }

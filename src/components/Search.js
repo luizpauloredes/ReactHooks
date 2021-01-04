567b60217db726ddb8ab83f0e52a0f5de77653f0
unknown <luizpaulo.redes@gmail.com>
2021-01-04 15:53:12 -0300

    search tool improvement

--- src/components/Search.js
+++ src/components/Search.js
@@ -23,18 +23,31 @@ const Search = () => {
 
         };
 
-        search();
+        const timeoutId = setTimeout(() => {
+            if (term) {
+                search();
+            }
+        }, 1500);
+
+        return () => {
+            clearTimeout(timeoutId)
+        }
+
     }, [term]);
 
 
     const renderedResults = results.map((result) => {
         return (
-            <div className="item">
+            <div key={result.pageid} className="item">
+
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
                     </div>
-                    {result.snippet}
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
                 </div>
             </div>
         )
